296d09929d4db920b13f3cd3274884706d4d563d
hosh <jolee934@gmail.com>
2022-08-14 20:10:22 -0700

    Add script.js

--- script.js
+++ script.js
@@ -13,16 +13,21 @@ function getComputerChoice() {
     return choice;
 } 
 
+let computerWinTotal= 0;
+let playerWinTotal = 0;
 
 //function to play a round 
 function playRound (playerSelection, computerSelection) {
     //note: this converts playerSection to uppercase to make comparison case insensitive
     let outcome;
+    
     if (playerSelection.toUpperCase() === "ROCK") {
         if (computerSelection.toUpperCase() === "PAPER") {
+            computerWinTotal++;
             return "You Lose! Paper beats Rock"
         }
         else if (computerSelection.toUpperCase() === "SCISSOR") {
+            playerWinTotal++;
             return "You Win! Rock beats Scissor"
         }
         else if (computerSelection.toUpperCase() === "ROCK") {
@@ -30,9 +35,11 @@ function playRound (playerSelection, computerSelection) {
         }
     }   else if (playerSelection.toUpperCase() === "PAPER") {
         if (computerSelection.toUpperCase() === "SCISSOR") {
+            computerWinTotal++;
             return "You Lose! Scissor beats Paper"
         }
         else if (computerSelection.toUpperCase() === "ROCK") {
+            playerWinTotal++;
             return "You Win! Paper beats Scissor"
         }
         else if (computerSelection.toUpperCase() === "PAPER") {
@@ -40,9 +47,11 @@ function playRound (playerSelection, computerSelection) {
        }
     } else if (playerSelection.toUpperCase() === "SCISSOR") {
         if (computerSelection.toUpperCase() === "ROCK") {
+            computerWinTotal++;
             return "You Lose! Rock beats Scissor"
         }
         else if (computerSelection.toUpperCase() === "PAPER") {
+            playerWinTotal++;
             return "You Win! Scissor beats Paper"
         }
         else if (computerSelection.toUpperCase() === "SCISSOR") {
@@ -51,17 +60,32 @@ function playRound (playerSelection, computerSelection) {
     }
 
 
-    /*    
-    } else if ((playerSelection.toUpperCase() === "PAPER")) {
-
-    } else if ((playerSelection.toUpperCase() === "SCISSOR"))
-    return outcome */
    
 }
 
-//testing the code
-const playerSelection = "scissor";
-const computerSelection = getComputerChoice();
-//testing computerSelection
+
+let playerSelection;
+let computerSelection;
+/*testing computerSelection
 console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+console.log(playRound(playerSelection, computerSelection))*/
+
+
+function game() {
+    for (let i = 0; i<5; i++) {
+        playerSelection = prompt("Type \"Rock\", \"Paper\", or \"Scissor\"")   
+        computerSelection = getComputerChoice(); 
+        console.log(computerSelection)    
+        console.log(playRound(playerSelection, computerSelection))
+    }
+    if (playerWinTotal > computerWinTotal) {
+        console.log(`You won! You scored ${playerWinTotal} and the computer scored ${computerWinTotal}`)
+    } else if (computerWinTotal > playerWinTotal) {
+        console.log(`You lose! You scored ${playerWinTotal} and the computer scored ${computerWinTotal}`)
+    } else {
+        console.log(`You tied! You scored ${playerWinTotal} and the computer scored ${computerWinTotal}`)
+    }
+
+} 
+
+game();
